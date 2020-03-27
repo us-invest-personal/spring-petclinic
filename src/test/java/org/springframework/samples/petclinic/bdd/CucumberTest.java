321f2bfff369093f4fbe15cd6ec9c29d50386739
José Antono Parejo <japarejo@gmail.com>
2020-03-27 03:20:38 +0100

    Cucumber scenarios working, still no pretty reporting.

--- src/test/java/org/springframework/samples/petclinic/bdd/CucumberTest.java
+++ src/test/java/org/springframework/samples/petclinic/bdd/CucumberTest.java
@@ -1,17 +1,24 @@
 package org.springframework.samples.petclinic.bdd;
 
-
+import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import io.cucumber.junit.Cucumber;
-
-
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/features",plugin = "json:target/cucumber-report.json")
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"stepdefs"},
+        tags = {"~@Ignore"},
+        plugin = {
+                "pretty",                                
+                "json",                
+                "html:target/cucumber-reports"
+        }, monochrome=true)
 public class CucumberTest {
 }
---
+++ src/test/java/org/springframework/samples/petclinic/bdd/stepdefinitions/LoginStepDefinitions.java
@@ -0,0 +1,67 @@
+package org.springframework.samples.petclinic.bdd.stepdefinitions;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.UUID;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import lombok.extern.java.Log;
+
+@Log
+public class LoginStepDefinitions extends AbstractStep {
+
+	@Given("I am not logged in the system")
+	public void IamNotLogged() throws Exception{
+		int port=8080;
+		getDriver().get("http://localhost:"+port);
+		WebElement element=getDriver().findElement(By.xpath("//div[@id='main-navbar']/ul[2]/li/a"));
+		if(element==null || !element.getText().equalsIgnoreCase("login")) {
+			getDriver().findElement(By.xpath("//div[@id='main-navbar']/ul[2]/li/a/span[2]")).click();
+			getDriver().findElement(By.linkText("Logout")).click();
+			getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+		}
+	}
+	
+	@When("I do login as admin1")
+	public void IdoLoginAs() throws Exception {
+		String username="admin1";
+		loginAs(username,passwordOf(username));		
+	}
+	
+	private void loginAs(String username,String password) {				
+		getDriver().findElement(By.xpath("//div[@id='main-navbar']/ul[2]/li/a")).click();
+		getDriver().findElement(By.id("password")).clear();
+		getDriver().findElement(By.id("password")).sendKeys(password);
+		getDriver().findElement(By.id("username")).clear();
+		getDriver().findElement(By.id("username")).sendKeys(username);
+		getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+	}
+	private String passwordOf(String username) {
+		return "4dm1n";
+	}
+
+	@Then("admin1 appears as the current user")
+	public void asCurretUserAppears() throws Exception {
+		String username="admin1";
+		assertEquals(username.toUpperCase(),
+				getDriver().findElement(By.xpath("//div[@id='main-navbar']/ul[2]/li/a/strong")).getText());
+		stopDriver();
+	}
+	
+	@When("I try to do login as admin1 with an invalid password")
+	public void ItryToDoLoginWithAnInvalidPasswordAs() throws Exception {
+		loginAs("admin1",UUID.randomUUID().toString());
+	}
+	
+	@Then("the login form is shown again")
+	public void theLoginFormIsShownAgain() throws Exception {
+		assertEquals(getDriver().getCurrentUrl(),"http://localhost:8080/login-error");
+		stopDriver();
+	}
+	
+}
