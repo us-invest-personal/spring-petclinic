229c5e80dfd5408a2603053c53a17c77b365e3da
José Antonio Parejo Maestre <japarejo@gmail.com>
2020-10-02 12:37:05 +0200

    Añadidos datos iniciales para las enfermedades y columna descripción

--- src/main/java/org/springframework/samples/petclinic/model/Disease.java
+++ src/main/java/org/springframework/samples/petclinic/model/Disease.java
@@ -1,10 +1,15 @@
 package org.springframework.samples.petclinic.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import lombok.Data;
+
 @Entity
+@Data
 @Table(name = "diseases")
 public class Disease extends NamedEntity{
-
+	@Column(length=1024)     // Needed in some environments for strings longer than 255 characters
+	private String description; 
 }
