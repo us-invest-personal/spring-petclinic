42ffce5f32336732bcaa2948d7e170ab996b23b9
José Antonio Parejo Maestre <japarejo@gmail.com>
2020-10-08 20:43:04 +0200

    Minimum description size of 10 characters.

--- src/main/java/org/springframework/samples/petclinic/model/Disease.java
+++ src/main/java/org/springframework/samples/petclinic/model/Disease.java
@@ -3,6 +3,7 @@ package org.springframework.samples.petclinic.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -10,6 +11,7 @@ import lombok.Data;
 @Data
 @Table(name = "diseases")
 public class Disease extends NamedEntity{
+	@Size(min = 10, max = 1024)
 	@Column(length=1024)     // Needed in some environments for strings longer than 255 characters
 	private String description; 
 }
