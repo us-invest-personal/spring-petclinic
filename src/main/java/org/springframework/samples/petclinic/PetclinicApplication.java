cd2dea9d3cdbe7c889368dd1f227ad16ce48239b
José Antonio Parejo Maestre <japarejo@gmail.com>
2020-12-07 14:47:02 +0100

    Logging configuration

--- src/main/java/org/springframework/samples/petclinic/PetclinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetclinicApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication()
 public class PetclinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetclinicApplication.class, args);
+		log.info("Awesomeness ahead!");
 	}
 
 }
