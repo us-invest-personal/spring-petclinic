48dc23970c55c66b51b99642acb0f3fd74da0b0b
Manuel Resinas <resinas@us.es>
2021-09-30 20:19:16 +0200

    Reorganized packages to a domain-oriented approach
    The packages are owner, pet, vet and user. We chose to keep pet out of owner because it is the central element of the application. However, visit is included in pet because a visit heavily depends on the pet that makes it. These were also the same packages that were used to organized the JSP code.

--- src/main/java/org/springframework/samples/petclinic/web/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/web/WelcomeController.java
@@ -3,10 +3,10 @@ package org.springframework.samples.petclinic.web;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.petclinic.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.samples.petclinic.model.User;
+import org.springframework.samples.petclinic.user.User;
+import org.springframework.samples.petclinic.user.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
